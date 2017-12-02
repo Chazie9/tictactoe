442f613f03fa81f9dd9f8ae1781fcbeca6ab1261
Chazie9 <charlesharring3@gmail.com>
2017-12-02 11:02:44 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+    <h1> hellow to the world!</h1>
+  </div>,
+  document.getElementById('root')
+)
+
+
+//   <App />, document.getElementById('root'))
+// registerServiceWorker()
